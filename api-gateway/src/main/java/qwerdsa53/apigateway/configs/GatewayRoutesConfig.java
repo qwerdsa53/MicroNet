ef526cd27c2953e5fd7a2c5c6a9094223cb35295
qwerdsa53 <sergeevae05@mail.ru>
2024-12-20 16:21:03 +0300

    test api-gateway cd

--- api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//1
+//12
 @Configuration
 public class GatewayRoutesConfig {
     @Value("${user.service.uri}")
