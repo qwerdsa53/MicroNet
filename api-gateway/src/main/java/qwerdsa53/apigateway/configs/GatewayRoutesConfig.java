01cb8b1de5731dd772cc911622d89c10fc649c3b
qwerdsa53 <qwerdsa53steam@gmail.com>
2024-12-20 10:25:58 +0300

    test ci-cd for all services

--- api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
@@ -5,10 +5,9 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//1
 @Configuration
 public class GatewayRoutesConfig {
-//q
     @Value("${user.service.uri}")
     private String userServiceUri;
 
--- feed-service/src/main/java/qwerdsa53/feeddervice/FeedController.java
+++ feed-service/src/main/java/qwerdsa53/feeddervice/FeedController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//1
 @Slf4j
 @RestController
 @RequestMapping("api/v1/feed")
