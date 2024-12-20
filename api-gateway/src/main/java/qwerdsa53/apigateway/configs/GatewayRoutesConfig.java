0290c83e1633430419b4d51e4d9121fe2dafddf8
qwerdsa53 <sergeevae05@mail.ru>
2024-12-20 09:49:07 +0300

    test api-gateway cd

--- api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
+++ api-gateway/src/main/java/qwerdsa53/apigateway/configs/GatewayRoutesConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GatewayRoutesConfig {
-//qq
+//q
     @Value("${user.service.uri}")
     private String userServiceUri;
 
