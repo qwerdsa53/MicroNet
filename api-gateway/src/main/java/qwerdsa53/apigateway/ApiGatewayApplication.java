c66e91cff88f29de72dd98c8e992680a60082d6c
qwerdsa53 <qwerdsa53steam@gmail.com>
2025-01-14 16:58:52 +0300

    test ci/cd conditions

--- api-gateway/src/main/java/qwerdsa53/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/qwerdsa53/apigateway/ApiGatewayApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
-    //1
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
